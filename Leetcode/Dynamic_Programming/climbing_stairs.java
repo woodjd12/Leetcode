26995e39394e1c257ad42a4a8b91ae241fba9c11
David Wood <woodjd12@yahoo.com>
2022-07-13 23:07:30 -0500

    First commit

---
+++ Leetcode/Dynamic_Programming/climbing_stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] stairs = new int[n+1];
+        stairs[1]  = 1;
+        stairs[2]  = 2;
+        for (int i = 3; i <= n; i++) {
+            stairs[i] = stairs[i - 1] + stairs[i - 2];
+        }
+        return stairs[n];
+        
+    }
+}
